76509fab95766e451819f4822c554900d1ec1473
Michael Moura <mikemorcerf@gmail.com>
2020-03-01 19:58:37 -0500

    first commit

---
+++ Tipsy/Models/Bill.swift
@@ -0,0 +1,9 @@
+//
+//  Bill.swift
+//  Tipsy
+//
+//  Created by Michael Moura on 2/16/20.
+//  Copyright © 2020 The App Brewery. All rights reserved.
+//
+
+import Foundation
---
+++ Tipsy/Models/CalculatorModel.swift
@@ -0,0 +1,9 @@
+//
+//  CalculatorModel.swift
+//  Tipsy
+//
+//  Created by Michael Moura on 2/16/20.
+//  Copyright © 2020 The App Brewery. All rights reserved.
+//
+
+import Foundation
